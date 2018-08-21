d505fa07c3e2831c5b01adf29cf75354a015e6f9
Juan Fox <foxj0875@gmail.com>
2018-08-21 12:21:21 -0400

    server.js responding properly to post request, body-parser middleware working as expected

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get('/', (req, res) => {
   res.send(`App is running`);
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body.name);
+  res.send(req.body.name);
 });
 
 
