07a4bfbecc7227d27d1cb8391d3075e554a60c49
Juan Fox <foxj0875@gmail.com>
2018-08-23 16:03:03 -0400

    React Router Links are navigating the browser correctly

--- client/src/components/Topbar/Topbar.js
+++ client/src/components/Topbar/Topbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Topbar = (props) => {
   return (
@@ -7,9 +8,9 @@ const Topbar = (props) => {
       <div className="title">JobSeeker</div>
       <nav>
         <ul>
-          <li>Home</li>
-          <li>JobSeeker</li>
-          <li>Recruiters</li>
+          <Link to="/"><li>Home</li></Link>
+          <Link to="/jobs"><li>JobSeeker</li></Link>
+          <Link to="recruiters"><li>Recruiters</li></Link>
         </ul>
       </nav>
     </div>
