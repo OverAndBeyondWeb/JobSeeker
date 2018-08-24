61a626dd8c94510b70d385ca4f70a77e9103ecc7
Juan Fox <foxj0875@gmail.com>
2018-08-24 08:11:38 -0400

    Topbar.js is generically styled

--- client/src/components/Topbar/Topbar.js
+++ client/src/components/Topbar/Topbar.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './Topbar.css';
+import logo from '../../images/jobs_icon.png';
+
 const Topbar = (props) => {
   return (
-    <div>
-      <div className="logo">Logo</div>
-      <div className="title">JobSeeker</div>
-      <nav>
-        <ul>
-          <Link to="/"><li>Home</li></Link>
-          <Link to="/jobs"><li>JobSeeker</li></Link>
-          <Link to="recruiters"><li>Recruiters</li></Link>
-        </ul>
-      </nav>
+    <div className="Topbar">
+      <div className="tb-container">
+        <div className="logo"><img src={logo} alt=""/></div>
+        <div className="title">JobSeeker</div>
+        <nav className="main-nav">
+          <ul>
+            <Link to="/"><li className="active">Home</li></Link>
+            <Link to="/jobs"><li>Jobs</li></Link>
+            <Link to="recruiters"><li>Recruiters</li></Link>
+          </ul>
+        </nav>
+      </div>
+      
     </div>
   )
 };
