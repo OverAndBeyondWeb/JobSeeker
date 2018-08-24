22f1c7c8b1249d909a3c79595e9004f68184d101
Juan Fox <foxj0875@gmail.com>
2018-08-24 08:55:49 -0400

    LandingPage.js generically styled

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './LandingPage.css';
 
 const LandingPage = (props) => {
   return (
-    <div>
-      This is the LandingPage
+    <div className="LandingPage">
+      <div className="content-box">
+        <h1>Job Seeker</h1>
+        <p>Having trouble keeping up with jobs that you have already applied for?  Job Seeker helps you track potential jobs as you apply!</p>
+        <button>Get Started!</button>
+      </div>
     </div>
   )
 };
--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -12,7 +12,6 @@ class Home extends Component {
   render() {
     return (
       <div>
-        This Is the Home Container
         <LandingPage/>
       </div>
     )
