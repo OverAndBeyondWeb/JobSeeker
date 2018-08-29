efedff723339a709ff12ebf9a2f40287585c7abe
Juan Fox <foxj0875@gmail.com>
2018-08-29 14:11:55 -0400

    server is connecting to mysql

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'job_tracker_db',
+  password: ''
+});
+
+conn.connect((err) => {
+  if(err) {
+    console.err(`connection error: ${err.stack}`);
+    return;
+  }
+
+  console.log(`db connection id: ${conn.threadId}`);
+});
 
 
 app.get('/', (req, res) => {
@@ -15,4 +32,4 @@ app.get('/', (req, res) => {
 app.use('/', require('./routes/apiRoutes'));
 
 
-app.listen(PORT, () => `App listening on port ${PORT}`);
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
