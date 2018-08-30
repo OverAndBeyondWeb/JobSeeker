53ffe7c89c6297ba99bf95ebe59f2300a896dff6
Juan Fox <foxj0875@gmail.com>
2018-08-30 12:29:06 -0400

    Bootstrap is linked to project by cdn from index.html

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -11,8 +11,11 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
+      <div className="Home">
         <LandingPage/>
+        <div className="container">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit impedit natus consectetur dolorum quam beatae obcaecati quo hic deleniti cumque, molestias architecto nostrum rerum perspiciatis facilis incidunt vitae? Possimus, animi expedita? Enim dolor at tempore deserunt porro sint iure quia amet ducimus dolorum praesentium sapiente alias quod delectus consequuntur aliquam, maxime ea assumenda. Provident, modi nulla magnam sit officia odit. Animi iste mollitia voluptas similique explicabo reprehenderit eum porro ex obcaecati, vero unde, architecto quam, temporibus quibusdam consequuntur earum quae vel praesentium iure nobis illum. Natus tempore corporis eius quia, laboriosam deleniti accusantium facilis odio vel animi quibusdam ad repudiandae?
+        </div>
       </div>
     )
   }
