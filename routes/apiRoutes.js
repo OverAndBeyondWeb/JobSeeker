6e9a6df064b94c88391d2f6133fa632eb992ac56
Juan Fox <foxj0875@gmail.com>
2018-08-30 12:04:10 -0400

    '/api/jobs' route is displaying results from querying the job table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
-router.get('/api/jobs', (req, res) => {
-  res.json({message: 'all jobs here'});
-});
+module.exports = (connection) => {
+  router.get('/api/jobs', (req, res) => {
+    const query = 'SELECT * FROM job';
+    connection.query(query, (err, results, fields) => {
+      if(err) throw err;
+      res.json(results);
+    });
+  });
 
-module.exports = router;
+  return router;
+}
--- server.js
+++ server.js
@@ -8,20 +8,20 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-const conn = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'job_tracker_db',
   password: ''
 });
 
-conn.connect((err) => {
+connection.connect((err) => {
   if(err) {
     console.err(`connection error: ${err.stack}`);
     return;
   }
 
-  console.log(`db connection id: ${conn.threadId}`);
+  console.log(`db connection id: ${connection.threadId}`);
 });
 
 
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
   res.send(`App is running`);
 });
 
-app.use('/', require('./routes/apiRoutes'));
+app.use('/', require('./routes/apiRoutes')(connection));
 
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
