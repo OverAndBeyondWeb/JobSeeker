5e375b8b8c60db36d223148fa83fc9ee5e6ccb6b
Juan Fox <foxj0875@gmail.com>
2018-09-02 23:44:02 -0400

    CompanyForm.js is inserting entries into the db correctly

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -54,14 +54,22 @@ module.exports = (connection) => {
   });
 
   router.post('/api/recruiters', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     res.send('ok');
   });
 
   router.post('/api/companies', (req, res) => {
-    console.log(req.body);
-    res.send('ok');
-  })
+    const rb = req.body;
+    let query = `
+      INSERT INTO company (name, web_link, location)
+      VALUES (?, ?, ?)
+    `;
+    connection.query(query, [rb.name, rb['web_link'], rb.location], (err, results, fields) => {
+      if(err) throw err;
+      console.log(results);
+      res.send('ok');
+    });
+  });
 
   return router;
 }
