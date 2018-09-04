17778f26e1c24e73bfdc9aff11d04116f58e481e
Juan Fox <foxj0875@gmail.com>
2018-09-04 12:44:05 -0400

    all controller files are renamed with a lowercase first letter

---
+++ controllers/jobs.js
@@ -0,0 +1,49 @@
+module.exports.getAllJobs = (req, res, connection) => {
+  const query = 'SELECT * FROM job';
+  connection.query(query, (err, results, fields) => {
+    if(err) throw err;
+    res.json(results);
+  });
+}
+
+module.exports.insertJob = (req, res, connection) => {
+  const rb = req.body;
+  const query = `
+    INSERT INTO job (job_title, job_number, job_link)
+    VALUES (?,?,?)
+  `;
+  const query1 = `
+    SELECT name FROM company
+  `;
+
+  const query2 = `
+    INSERT INTO company (name)
+    VALUES (?)
+  `;
+  
+
+  connection.query(query1, (err, results, fields) => {
+    if(err) throw err;
+    console.log(rb['company_name']);
+    console.log(results.map(result => result.name));
+    if (results.map(result => result.name).indexOf(rb['company_name']) === -1) {
+      connection.query(query2, [rb['company_name']], (err, results, fields) => {
+        if(err) throw err;
+        connection.query(query, [rb['title'], rb['job_number'], rb['job_link']], (err, results, fields) => {
+          if(err) throw err;
+          console.log(results);
+          res.send('ok');
+        });
+        console.log(query);
+      });
+    }else {
+      connection.query(query, [rb['title'], rb['job_number'], rb['job_link']], (err, results, fields) => {
+        if(err) throw err;
+        console.log(results);
+        console.log('already inserted');
+        res.send('ok');
+      });
+      console.log(query);
+    }
+  });
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,53 +1,11 @@
 const router = require('express').Router();
-const jobsController = require('../controllers/Jobs');
+const jobsController = require('../controllers/jobs');
 
 module.exports = (connection) => {
 
   router.get('/api/jobs', (req, res) => {
     jobsController.getAllJobs(req, res, connection);
   });
-
-  // router.post('/api/jobs', (req, res) => {
-  //   const rb = req.body;
-  //   const query = `
-  //     INSERT INTO job (job_title, job_number, job_link)
-  //     VALUES (?,?,?)
-  //   `;
-  //   const query1 = `
-  //     SELECT name FROM company
-  //   `;
-
-  //   const query2 = `
-  //     INSERT INTO company (name)
-  //     VALUES (?)
-  //   `;
-   
-
-  //   connection.query(query1, (err, results, fields) => {
-  //     if(err) throw err;
-  //     console.log(rb['company_name']);
-  //     console.log(results.map(result => result.name));
-  //     if (results.map(result => result.name).indexOf(rb['company_name']) === -1) {
-  //       connection.query(query2, [rb['company_name']], (err, results, fields) => {
-  //         if(err) throw err;
-  //         connection.query(query, [rb['title'], rb['job_number'], rb['job_link']], (err, results, fields) => {
-  //           if(err) throw err;
-  //           console.log(results);
-  //           res.send('ok');
-  //         });
-  //         console.log(query);
-  //       });
-  //     }else {
-  //       connection.query(query, [rb['title'], rb['job_number'], rb['job_link']], (err, results, fields) => {
-  //         if(err) throw err;
-  //         console.log(results);
-  //         console.log('already inserted');
-  //         res.send('ok');
-  //       });
-  //       console.log(query);
-  //     }
-  //   });
-  // });
 
   router.post('/api/jobs', (req, res) => {
     jobsController.insertJob(req, res, connection);
