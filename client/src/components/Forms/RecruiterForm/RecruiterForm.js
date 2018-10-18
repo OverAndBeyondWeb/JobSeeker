e256b260618e8588bf9393c9903ee6d5d651e786
Juan Fox <foxj0875@gmail.com>
2018-10-18 13:41:27 -0400

    RecruiterForm.js component is now a class component

--- client/src/components/Forms/RecruiterForm/RecruiterForm.js
+++ client/src/components/Forms/RecruiterForm/RecruiterForm.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './RecruiterForm.css';
 
-const RecruiterForm = (props) => {
-  return (
-    <form className="RecruiterForm container">
+class RecruiterForm extends Component {
 
-      <label htmlFor="fname">First Name</label>
-      <input id="fname" name="fname" type="text" onChange={props.handleChange}/>
-
-      <label htmlFor="lname">Last Name</label>
-      <input id="lname" name="lname" type="text" onChange={props.handleChange}/>
-
-      <label htmlFor="title">Title</label>
-      <input id="title" name="title" type="text" onChange={props.handleChange}/>
-
-      <label htmlFor="internal">Internal?</label>
-      <input id="internal" name="internal" type="text" onChange={props.handleChange}/>
-
-      <label htmlFor="email">Email Address</label>
-      <input id="email" name="email" type="text" onChange={props.handleChange}/>
-
-      <label htmlFor="phone">Phone</label>
-      <input id="phone" name="phone" type="text" onChange={props.handleChange}/>
-      
-      <label htmlFor="company_name">Company Name</label>
-      <input id="company_name" name="company_name" type="text" onChange={props.handleChange}/>
-
-      <button type="submit" onClick={props.submitForm}>Submit</button>
-    </form>
-  );
+  render() {
+    return (
+      <form className="RecruiterForm container">
+  
+        <label htmlFor="fname">First Name</label>
+        <input id="fname" name="fname" type="text" onChange={this.props.handleChange}/>
+  
+        <label htmlFor="lname">Last Name</label>
+        <input id="lname" name="lname" type="text" onChange={this.props.handleChange}/>
+  
+        <label htmlFor="title">Title</label>
+        <input id="title" name="title" type="text" onChange={this.props.handleChange}/>
+  
+        <label htmlFor="internal">Internal?</label>
+        <input id="internal" name="internal" type="text" onChange={this.props.handleChange}/>
+  
+        <label htmlFor="email">Email Address</label>
+        <input id="email" name="email" type="text" onChange={this.props.handleChange}/>
+  
+        <label htmlFor="phone">Phone</label>
+        <input id="phone" name="phone" type="text" onChange={this.props.handleChange}/>
+        
+        <label htmlFor="company_name">Company Name</label>
+        <input id="company_name" name="company_name" type="text" onChange={this.props.handleChange}/>
+  
+        <button type="submit" onClick={this.props.submitForm}>Submit</button>
+      </form>
+    );
+  } 
 };
 
 export default RecruiterForm;
